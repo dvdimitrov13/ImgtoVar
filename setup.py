c12885729f4e311e1bd3fb27ef9c2ed5957b3a73
Dimitar <dvdimitrov13@gmail.com>
2022-07-05 17:58:19 +0300

    Created a setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="imgtovar",
+    version="0.0.1",
+    description="",
+    long_description="",
+    url="https://www.example.com",
+    author="dvdimitrov13",
+    author_email="dvdimitrov13@gmail.com",
+    packages=[],
+    classifiers=["Development Status :: 1 - Cleanning up"],
+)
