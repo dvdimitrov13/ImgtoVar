48505b96db5e6a8020028c36852fb2c3f53c75cb
Dimitar <dvdimitrov13@gmail.com>
2022-08-29 15:47:44 +0300

    test

--- tests/Data_clean.py
+++ tests/Data_clean.py
@@ -5,9 +5,9 @@ sys.path.insert(0, "/home/dimitar/Documents/Thesis_research/ImgtoVar")
 
 from imgtovar import ImgtoVar
 
-df = ImgtoVar.color_analysis("./extract_output", extract=True, resume=True)
+# df = ImgtoVar.color_analysis("./extract_output", extract=True, resume=True)
 # df = ImgtoVar.detect_infographics("./extract_output", extract=True, resume=True)
-df = ImgtoVar.detect_invertedImg("./tests/test_data copy", extract=True)
+df = ImgtoVar.detect_invertedImg("./extract_output", extract=True)
 
 
 print(df.head())
